8f2a517d272fc8f38312fb15c0e2ecd0e6360d8e
Dario Sumlin <dsumlin@focusbrands.com>
2025-03-31 23:08:06 -0400

    updated redis manager host and port

--- api/script/redis-manager.ts
+++ api/script/redis-manager.ts
@@ -100,8 +100,8 @@ export class RedisManager {
   constructor() {
     if (process.env.REDIS_HOST && process.env.REDIS_PORT) {
       const redisConfig = {
-        host: process.env.REDIS_HOST,
-        port: process.env.REDIS_PORT,
+        host: '20.46.233.210',
+        port: 6379,
         // auth_pass: process.env.REDIS_KEY,
         // tls: {
         //   // Note: Node defaults CA's to those trusted by Mozilla
